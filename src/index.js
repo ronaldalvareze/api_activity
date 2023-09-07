9644b9b379363308c6c00f676cee5715cc9d6f4e
ronaldalvareze <127575929+ronaldalvareze@users.noreply.github.com>
2023-09-07 17:25:31 -0500

    actividad de api rest para el despliegue

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import employeesRoutes from "./routes/employees.routes.js"
+import indexRoutes from "./routes/index.routes.js"
+
+const app = express()
+
+app.use(express.json())
+
+app.use(indexRoutes)
+app.use("/api",employeesRoutes)
+
+
+app.listen(3000)
+
+console.log("servidor en puerto http://localhost:3000");
